e2d18bdcb1401f465ae5cde1e1f2c21e6992bd50
vions <vions323@gmail.com>
2023-08-08 09:56:20 +0200

    Printing a date

---
+++ src/Date.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Date {
+    public static void main(String[] args) {
+
+        //Написать метод,, который принимает 3 числа int (day, month, year) возвращает строку вида "08. August 2023 "
+
+
+
+
+
+
+
+
+
+    }
+    //===Methods===
+    static Scanner scanner = new Scanner(System.in);
+    public static double day (){
+        System.out.println("Enter a day:  ");
+        return scanner.nextInt();
+    }
+    public static double month (){
+        System.out.println("Enter a month:  ");
+
+        switch ()
+        return scanner.nextInt();
+
+
+    //=============
+}
